50af93cd9236649aae40dfaafa605d9cf605f967
udayvarma <109035820+uday290302@users.noreply.github.com>
2024-02-13 00:10:41 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -13,7 +13,8 @@ const USER = require("../models/user");
 router.get("/", async(req,res)=>{
   // const  
     const shortUrls =await shortUrl.find({ createdBy: req.user._id });
-    const Users = req.user.name;
+    const names = await USER.findOne({ _id: req.user._id });
+     const Users = names.name;
    console.log(Users)
 res.render('index', { shortUrls: shortUrls  ,Users:Users})
 });
